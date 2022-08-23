34452c24d492c9700fa8da11ab56d19d385a38db
alsoson <101792855+alsoson@users.noreply.github.com>
2022-08-23 05:30:14 +0000

    Deploying to gh-pages from @ alsoson/halal-front@80acdde6d40d70b00e0c1aece96a2750cc0120e9 🚀

--- assets/OrderHistoryPage.681c050a.js
+++
@@ -1,1 +0,0 @@
-import{bh as r,m as e,af as o}from"./index.872c6a74.js";const t={};function a(c,n){return e(),o("h1",null,"order History")}var _=r(t,[["render",a]]);export{_ as default};
---
+++ assets/OrderHistoryPage.763fde47.js
@@ -0,0 +1,1 @@
+import{bh as r,m as e,af as o}from"./index.e0dd589b.js";const t={};function a(c,n){return e(),o("h1",null,"order History")}var _=r(t,[["render",a]]);export{_ as default};
