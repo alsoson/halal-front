f77a08a1ff60d51af4ed17ab22f8c8dbd3ac194b
alsoson <101792855+alsoson@users.noreply.github.com>
2022-08-23 03:34:56 +0000

    Deploying to gh-pages from @ alsoson/halal-front@26853c23aad112b5dc96d41b9d03bfb16a667dc0 🚀

---
+++ assets/QCardActions.92fd92e8.js
@@ -0,0 +1,1 @@
+import{a as t,bb as r,bc as c,d as n,h as l,e as i}from"./index.872c6a74.js";var d=t({name:"QCardActions",props:{...r,vertical:Boolean},setup(a,{slots:s}){const e=c(a),o=n(()=>`q-card__actions ${e.value} q-card__actions--${a.vertical===!0?"vert column":"horiz row"}`);return()=>l("div",{class:o.value},i(s.default))}});export{d as Q};
